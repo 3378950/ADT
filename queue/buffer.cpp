132484b67b2264989879a2c0560369e2ea02ec46
hot_machine <48776657+3378950@users.noreply.github.com>
2019-08-15 16:08:11 +0800

    Add files via upload

---
+++ queue/buffer.cpp
@@ -0,0 +1,69 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <list>
+#include <forward_list>
+#include <map>
+#include <random>
+
+
+using namespace std;
+
+template<typename T>
+
+void print_all(const T&data){
+	for(const auto&x:data)
+		cout<<x<<" ";
+	cout<<endl;
+}
+
+
+int main()
+{
+	vector<int> buffer;
+	int C = 10;//capacity
+	int N = C+1;
+	buffer.resize(N);
+	int x = 0;
+	int front = N/2;//任选有效位置
+	int rear = N/2;
+	while(rear + 1 < N ? rear + 1 != front : 0 != front)//buffer不满，连续放入
+	{
+		buffer[rear] = x++;
+		//rear= (rear+1)%N
+		if(rear<C) rear++;
+		else rear = 0;
+	}
+	while(front!=rear)//buffer非空
+	{
+		cout<<buffer[front]<<" ";
+		//front = (front+1)%N; 
+		if(front<C) ++front;
+		else front = 0;
+	}
+	cout<<endl;
+
+	queue<int>Q;
+	for(int x = 0;Q.size()!=C;x++){
+		Q.push(x);
+	}
+	while(!Q.empty())
+	{
+		cout<<Q.front()<<" ";
+		Q.pop();
+	}
+	cout<<endl;
+
+    system("pause");
+    return 0;
+} 
+
---
+++ stack/Binary_Conversion.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <set>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <deque>
+#include <algorithm>
+#include <string>
+#include <cstring>
+#include <cstdlib>
+#include <ctime>
+#include <cctype>
+#include <list>
+#include <forward_list>
+#include <map>
+#include <random>
+
+
+using namespace std;
+
+template<typename T>
+
+void print_all(const T&data){
+	for(const auto&x:data)
+		cout<<x<<" ";
+	cout<<endl;
+}
+void convertor_with_stack(int number,size_t b){
+	string d2c = "0123456789ABCDEF";//max = 16
+	string result;
+	stack<char> S;
+	if(number<0){
+		result+='-';
+		number = -number;
+	}
+	do{
+		S.push(d2c[number%b]);
+		number/=b;
+	}while(number!=0);
+	while(!S.empty()){
+		result+=S.top();
+		S.pop();
+	}
+	print_all(result);
+}
+
+
+void convertor_with_string(int number,size_t b){
+	string result;
+	string d2c = "0123456789ABCDEF";//max = 16
+	int start = 0;
+	if(number<0){
+		result+='-';
+		number = -number;
+		start = 1;
+	}
+	do{
+		result+=d2c[number%b];
+		number/=b;
+	}while(number!=0);
+	for(int i=start;i<(result.size()+start)/2;i++){
+		swap(result[i],result[result.size()-i-1+start]);
+	}
+	print_all(result);
+
+}
+int main()
+{
+    int number,b;
+	cout<<"number: ";//Numbers requiring conversion
+	cin>>number;
+	cout<<"b:";//the converted process (between 2 and 16)
+	cin>>b;
+	convertor_with_stack(number,b);
+	convertor_with_string(number,b);
+    system("pause");
+    return 0;
+} 
+
