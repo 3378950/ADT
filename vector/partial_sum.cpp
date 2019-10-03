3b5941fb7bd4b0671585ba40a41dc7178584ceac
hot_machine <48776657+3378950@users.noreply.github.com>
2019-10-03 16:03:57 +0800

    Update partial_sum.cpp

--- vector/partial_sum.cpp
+++ vector/partial_sum.cpp
@@ -22,7 +22,7 @@ int main()
 	vector<int> points = {0,46,56,61,75,86,101};
 	if(points.size()<2) return 0;
 	vector<int>nums(points.size()-1,0);
-	//
+	
 	for(size_t i=0;i<nums.size();++i)
 	{
 		for(size_t j=points[i];j<points[i+1];++j)
