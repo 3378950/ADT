6c06ef5d9d3beea6ac9457642853b9fa7d7e3fe6
hot_machine <48776657+3378950@users.noreply.github.com>
2019-12-17 20:44:03 +0800

    Update 3n+1.cpp

--- Algorithm/3n+1.cpp
+++ Algorithm/3n+1.cpp
@@ -6,16 +6,16 @@ using namespace std;
 size_t memorized_Collatz(vector<size_t> &v,size_t n) 
 {
 	size_t D = 0;
-	while(n>=v.size())
+	while(n >= v.size())
 	{
-		n = (n%2==0)?n/2:3*n+1;
+		n = (n%2 == 0) ? n/2 : 3 * n + 1;
 		++D;
 	}
 	if(v[n]==0&&n>0)
 	{
-		v[n] = memorized_Collatz(v,(n%2==0)?n/2:3*n+1)+1;
+		v[n] = memorized_Collatz(v,(n%2==0) ? n / 2 : 3 * n + 1) + 1;
 	}
-	return v[n]+D;
+	return v[n] + D;
 }
 int main()
 {
@@ -23,9 +23,9 @@ int main()
 	vector<size_t> v (m,0);
 	v[1] = 1;
 	int x = memorized_Collatz(v,5);
-	cout<<x<<endl;
-	for(int i=0;i<v.size();++i)
-	cout<<v[i]<<" ";
-	cout<<endl;
+	cout << x << endl;
+	for(int i = 0; i < v.size(); ++i)
+	cout << v[i] << " ";
+	cout << endl;
 	return 0;
 }
