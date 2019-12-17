1e1a529ee943318a8f0228d9862fdb3602c8f8cd
hot_machine <48776657+3378950@users.noreply.github.com>
2019-12-17 20:48:17 +0800

    Update Logical Judgment - Who Has Doed Good.c

--- Algorithm/Logical Judgment - Who Has Doed Good.c
+++ Algorithm/Logical Judgment - Who Has Doed Good.c
@@ -1,18 +1,19 @@
 #include<stdio.h>
+
 int main()
 {
-	int i,sum=0,n,flag=0;
+	int i,sum = 0,n,flag = 0;
 	char man;
 	scanf("%d",&n);
-	for(i=0;i<6;i++)
+	for(i = 0; i < 6; i++)
 	{
 		
-		man='A'+i;
-		sum=(man!='A')+(man=='C')+(man=='D')+(man!='D')+(man=='C'||man=='E')+(man!='D'&&man!='E');
-		if(sum==n)
+		man = 'A' + i;
+		sum = (man != 'A') + (man == 'C') + (man == 'D') + (man != 'D') + (man ==' C' || man == 'E') + (man != 'D' && man != 'E');
+		if(sum == n)
 		{
 			flag++;
-			if(flag>1)
+			if(flag > 1)
 			{
 				printf(" or %c",man);
 			}
@@ -22,7 +23,7 @@ int main()
 			}
 		}
 	}
-	if(flag==0)
+	if(flag == 0)
 	printf("Error");
 	return 0;
 }
