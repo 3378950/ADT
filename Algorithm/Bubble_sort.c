ad9b949738bf01518ef9ebd8c73027416c4eb5a2
hot_machine <48776657+3378950@users.noreply.github.com>
2019-12-17 20:45:45 +0800

    Update and rename Bubble sort.c to Bubble_sort.c

--- Algorithm/Bubble_sort.c
+++ Algorithm/Bubble_sort.c
@@ -6,24 +6,26 @@ int main()
 	int n;
 	int a[100];
 	scanf("%d",&n);
-	for(i=0;i<n;i++)
-	scanf("%d",&a[i]);
+	for(i=0;i<n;i++) scanf("%d",&a[i]);
+	
 	Bubble(a,n);
+	
 	for(i=0;i<n;i++)
 	printf("%d ",a[i]);
+	
 	return 0;
  } 
  void Bubble(int a[],int n)
  {
  	int t;
- 	for(i=1;i<n;i++)
- 		for(j=0;j<n-i;j++)
+ 	for(i = 1; i < n; i++)
+ 		for(j = 0;j < n - i; j++)
  		{
- 			if(a[j]>a[j+1])
+ 			if(a[j] > a[j+1])
  			{
- 				t=a[j];
- 				a[j]=a[j+1];
- 				a[j+1]=t;
+ 				t = a[j];
+ 				a[j] = a[j + 1];
+ 				a[j + 1] = t;
 			}
 		}
  }
