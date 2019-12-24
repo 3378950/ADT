722595d4164aa63b01f4479994751ef409ab5b4f
hot_machine <48776657+3378950@users.noreply.github.com>
2019-12-24 23:17:36 +0800

    change the format

--- forward_list/Continuous_storage_test.cpp
+++ forward_list/Continuous_storage_test.cpp
@@ -1,44 +1,36 @@
 #include<iostream>
-#include<set>
-#include<vector>
 #include<algorithm>
-#include<string>
-#include<cstring>
 #include<cstdlib>
 #include<ctime>
-#include<cctype>
-#include<list>
 #include<forward_list>
 
 using namespace std;
 
 template<typename T>
 
-void print_all(const T&data){
+void print_all(const T&data) {
 	for(const auto&x:data)
 		cout<<x<<" ";
 	cout<<endl;
 }
 
 
-inline double time(clock_t start, clock_t end)
-{
+inline double time(clock_t start, clock_t end) {
   return static_cast<double>(end - start) / static_cast<double>(CLOCKS_PER_SEC);
 }
 
-struct snode{
+struct snode {
     int data;
     snode* next;
 };
 
-int main()
-{
+int main() {
     const size_t n = 100000000;//8G内存使用率96%
     //handwriting_push_back
     clock_t start1 = clock();
     snode* header = new snode;//动态申请空间
 	snode* p = header;
-    for(size_t i=0;i<n;++i){
+    for(size_t i = 0; i < n; ++i) {
         snode* next = new snode;
         p->next = next;
         p = next;
@@ -49,7 +41,7 @@ int main()
     cout <<"time1 is  "<< time(start1, end1) << endl;
 
     p = header;
-    while(p!=NULL){
+    while(p != NULL){
         snode *next = p->next;
         delete p;
         p = next;
@@ -57,14 +49,18 @@ int main()
     //push_back
     forward_list<int> FL1;
     clock_t start2 = clock();
+    
     auto iter = FL1.before_begin();
-    for(size_t i = 0;i<n;i++)   iter = FL1.insert_after(iter,0);
+    for(size_t i = 0;i < n; i++)   iter = FL1.insert_after(iter, 0);
     clock_t end2 = clock();
+    
     int b = time(start2, end2);
     cout <<"time2 is  "<< time(start2, end2) << endl;
+    
     //push_front
     forward_list<int> FL2;
     clock_t start3 = clock();
+    
     for (size_t i = 0; i < n; ++i)
     FL2.push_front(0);
     clock_t end3 = clock();
